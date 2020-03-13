d7b85b7c9bd21b0c7bdcf4e9d0fc23d36af2dcb9
qiushuang139 <429749476@qq.com>
2020-03-13 18:00:14 +0800

    Signed-off-by: qiushuang139 <429749476@qq.com>

---
+++ CSharpWeek4-Clock/CSharpWeek4-Clock/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+
+namespace CSharpWeek4_Clock
+{
+    //响铃
+    public class Alarm
+    {
+        //响铃
+        public void Belling(object sender, ReachTimeEventArgs e)
+        {
+            Clock w = (Clock)sender;
+            Console.WriteLine("time:"+w.Time+" didi ");
+        }
+
+        //按下任意键后响铃终止
+        public void Press(object sender,ReachTimeEventArgs e)
+        {
+            Clock w = (Clock)sender;
+            string str = Console.ReadLine();
+            while (str == "\n")
+            {
+
+            }
+            Console.WriteLine("power off");
+            w.Time=(w.Time)+1;
+            //Console.WriteLine(w.Time);
+            w.Work();
+        }
+    }
+
+
+    public class ReachTimeEventArgs : EventArgs
+    {
+        public int time;
+        public ReachTimeEventArgs(int t)
+        {
+            time = t;
+        }
+    }
+
+    //声明一个委托类型
+    public delegate void ReachTimeEventHandle(object sender, ReachTimeEventArgs e);
+
+    //闹钟
+    public class Clock
+    {
+        public event ReachTimeEventHandle ReachTime;//定义事件
+
+        public Alarm alarm;
+        public Clock()
+        {
+            this.alarm = new Alarm();
+        }
+
+        //默认开始事件为0
+        private int time = 0;
+        private int alarmTime;
+        public int Time
+        {
+            get{ return time; }
+            set{ time = value; }
+        }
+        public void SetAlarmTime(int alarmTime)
+        {
+            this.alarmTime = alarmTime;
+        }
+        
+
+        //主函数
+        public void Work()
+        {
+            while (time !=this.alarmTime)//未到响铃时间时或者超过响铃时间时，时间增加
+            {
+               
+                Console.WriteLine("time:"+time+" tick");
+                System.Threading.Thread.Sleep(1000);
+                time = time + 1;
+            }
+            ReachTimeEventArgs e = new ReachTimeEventArgs(this.time);
+            ReachTime(this, e);//触发事件
+        }
+    }
+
+   
+    class Program
+    {
+        static void ClockTest()
+        {
+
+            Clock w = new Clock();
+            w.SetAlarmTime(2);
+            w.ReachTime += w.alarm.Belling;
+            w.ReachTime += w.alarm.Press;
+            w.Work();
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            ClockTest();
+        }
+    }
+}
---
+++ CSharpWeek4-List/CSharpWeek4/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpWeek4
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = this.head;
+            while (node != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+        }
+        
+    }
+
+
+    class Program
+    {
+        public static void GenericListTest()
+        {
+            GenericList<int> list = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            //print list
+            list.ForEach(i => Console.WriteLine(i));
+
+            int max = int.MinValue;
+            list.ForEach(i => { if (max < i) max = i; });
+            Console.WriteLine("Max:" + max);
+
+            int min = int.MaxValue;
+            list.ForEach(i => { if (min > i) min = i; });
+            Console.WriteLine("Min:" + min);
+
+            int sum = 0;
+            list.ForEach(i => { sum += i; });
+            Console.WriteLine("Sum:" + sum);
+        }
+
+        public static void ClockTest()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            GenericListTest();
+        }
+    }
+
+
+}
